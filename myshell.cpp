8fe4403ab4e5a7aea438b3967e673647e6b831fc
MrJuano <jdm161@students.uwf.edu>
2023-09-11 19:18:55 -0500

    Debug mode

--- myshell.cpp
+++ myshell.cpp
@@ -2,18 +2,23 @@
 
 int main(){
 	Param *par = NULL;
+	int debugMode = 0;
+
 	while(true){
 		char input[500];
 		std::cout << "$$$ ";
-		std::cin.getline(input, 500);
+		std::cin.getline(input, 1500);
+		par = new Param(input);
 
 		if (strcmp(input, "exit") == 0){
 			break;		
 		}
 
-		par = new Param(input);
-	
-		par->printParams();
+		if(debugMode == 1)
+			par->printParams();
+
+		if(strcmp(input, "-Debug") == 0)
+			debugMode = 1;
 	}
 
 	return 0;
