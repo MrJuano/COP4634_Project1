ac9c34210ce0446e1909b4006e2b0380b4782049
MrJuano <jdm161@students.uwf.edu>
2023-09-11 21:21:43 -0500

    printParam() functionality

--- myshell.cpp
+++ myshell.cpp
@@ -1,24 +1,36 @@
 #include "parse.hpp"
 
+void printInput(char input[500]){
+	Param *par = new Param(input);
+	par->printParams();
+}
+
 int main(){
 	Param *par = NULL;
 	int debugMode = 0;
+	
+	char input[500];
+	char inputBuffer[500];
 
 	while(true){
-		char input[500];
 		std::cout << "$$$ ";
 		std::cin.getline(input, 1500);
-		par = new Param(input);
 
 		if (strcmp(input, "exit") == 0){
 			break;		
 		}
+		
+		if(strcmp(input, "printParam()") == 0){
+			printInput(inputBuffer);
 
-		if(debugMode == 1)
-			par->printParams();
-
+		}
+		else if(debugMode == 1)
+			printInput(input);
+		
 		if(strcmp(input, "-Debug") == 0)
 			debugMode = 1;
+		else
+			strcpy(inputBuffer, input);
 	}
 
 	return 0;
